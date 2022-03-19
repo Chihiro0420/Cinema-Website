c712402033f9d4822ea7699ccf59ea6413a97abe
Chihiro0420 <chihiroe2510@gmail.com>
2022-03-20 11:18:59 +1300

    add footer.js

---
+++ page/footer.js
@@ -0,0 +1,10 @@
+function footer(){
+  let html = ''
+  html += '<ul class="footer-flex">'
+  html += '<li>'
+  html += '<a href="contact.html">&copy; 2021 AKL Cinema</a>'
+  html += '</li>'
+  html += '<li><a href="../page/privacy.html">Privacy Policy</a></li>'
+  html += '</ul> '
+  document.write(html)
+}
